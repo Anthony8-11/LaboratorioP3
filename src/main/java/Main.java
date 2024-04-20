84d743a2801ba20cb11eab5366b60040d0a8aa84
Anthony Hernandez <anthonyrhernandez6@gmail.com>
2024-04-20 08:06:30 -0600

    Initial

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        // Creamos una instancia de PersonaCRUD para interactuar con la base de datos
+        PersonaCRUD personaCRUD = new PersonaCRUD();
+
+        // Insertar personas
+        personaCRUD.insertarPersona("Juan", 30, "Ciudad A");
+        personaCRUD.insertarPersona("María", 25, "Ciudad B");
+
+        // Mostrar personas
+        System.out.println("Personas:");
+        personaCRUD.mostrarPersonas();
+
+        // Actualizar persona
+        personaCRUD.actualizarPersona("Juan", "Juan Pérez");
+
+        // Mostrar personas actualizadas
+        System.out.println("\nPersonas después de la actualización:");
+        personaCRUD.mostrarPersonas();
+
+        // Eliminar persona
+        personaCRUD.eliminarPersona("María");
+
+        // Mostrar personas después de eliminar
+        System.out.println("\nPersonas después de la eliminación:");
+        personaCRUD.mostrarPersonas();
+    }
 }
