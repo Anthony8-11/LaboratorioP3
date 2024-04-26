143f6c8c69a3aff6b51ae6241f91e22799ac4ea5
Anthony Hernandez <anthonyrhernandez6@gmail.com>
2024-04-26 16:38:23 -0600

    Initial

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,27 +3,27 @@ public class Main {
         // Creamos una instancia de PersonaCRUD para interactuar con la base de datos
         PersonaCRUD personaCRUD = new PersonaCRUD();
 
-        // Insertar personas
-        personaCRUD.insertarPersona("Yeison", 30, "Jutiapa");
-        personaCRUD.insertarPersona("Martin", 25, "Guatemala");
-        personaCRUD.insertarPersona("ElEx", 35, "Cuba");
-        personaCRUD.insertarPersona("Benito", 50, "Rusia");
+//        // Insertar personas
+//        personaCRUD.insertarPersona("Yeison", 30, "Jutiapa");
+//        personaCRUD.insertarPersona("Martin", 25, "Guatemala");
+//        personaCRUD.insertarPersona("ElEx", 35, "Cuba");
+//        personaCRUD.insertarPersona("Benito", 50, "Rusia");
 
 
-        // Mostrar personas
-        System.out.println("Personas:");
-        personaCRUD.mostrarPersonas();
+//        // Mostrar personas
+//        System.out.println("Personas:");
+//        personaCRUD.mostrarPersonas();
 
         // Actualizar persona
-        personaCRUD.actualizarPersona("Yeison", "Yeison López");
-
-        // Mostrar personas actualizadas
-        System.out.println("\nPersonas después de la actualización:");
-        personaCRUD.mostrarPersonas();
-
-        // Eliminar persona
-        personaCRUD.eliminarPersona("ElEx");
-
+        //personaCRUD.actualizarPersona("Yeison", "Yeison López");
+//
+//        // Mostrar personas actualizadas
+//        System.out.println("\nPersonas después de la actualización:");
+//        personaCRUD.mostrarPersonas();
+//
+//        // Eliminar persona
+//        personaCRUD.eliminarPersona("ElEx");
+//
         // Mostrar personas después de eliminar
         System.out.println("\nPersonas después de la eliminación:");
         personaCRUD.mostrarPersonas();
